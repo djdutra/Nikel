e23aed9d7c283be872bee8f8838680d113ec5378
Daniel Jose Dutra <djdutra@gmail.com>
2022-01-31 16:33:22 -0300

    Iniciando o GIT

---
+++ public/js/indextestes.js
@@ -0,0 +1,10 @@
+const nome = "Marcelo Eltz";
+let nome2 = "Marcelo Eltz";
+
+console.log("Valor inicial:");
+console.log(nome2);
+
+nome2 = "Pedro Silva";
+
+console.log("Valor alterado:");
+console.log(nome2);
